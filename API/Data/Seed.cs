c7232b5694f5f5726ed7babec4f680f223e7cb04
yogesh nikam <yogeshk04@gmail.com>
2021-02-06 10:18:21 +0530

    User Manager update

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -4,26 +4,26 @@ using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 using API.Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
 {
     public class Seed
     {
-        public static async Task SeedUsers(DataContext context)
+        public static async Task SeedUsers(UserManager<AppUser> userManager)
         {
-            if (await context.Users.AnyAsync()) return;
+            if (await userManager.Users.AnyAsync()) return;
 
             var userData = await System.IO.File.ReadAllTextAsync("Data/UserSeedDatal.json");
             var users = JsonSerializer.Deserialize<List<AppUser>>(userData);
             if (users == null) return;
+            
             foreach (var user in users)
             {
                 user.UserName = user.UserName.ToLower();                
-                await context.Users.AddAsync(user);
+                await userManager.CreateAsync(user, "pass");
             }
-
-            await context.SaveChangesAsync();
         }
     }
 }
